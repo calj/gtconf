bec6a75009566e514c55cc22626e31bd6d720d80
Camille Meulien <c@ant.com>
2009-05-11 16:38:47 +0200

    Fixed emacs problems in no windows mode

---
+++ emacs/konsole-binds.el
@@ -0,0 +1,21 @@
+;;; konsole-binds.el ---
+
+;; Copyright (C) 2009
+;; Project: Geektips.Org sharing dot files system
+;; Author:  <camille.meulien@gmail.com>
+
+(provide 'konsole-binds)
+
+(define-key function-key-map "\e[H"    [C-home])
+(define-key function-key-map "\e[F"    [C-end])
+(define-key function-key-map "\e[A"    [C-up])
+(define-key function-key-map "\e[B"    [C-down])
+(define-key function-key-map "\e[D"    [C-left])
+(define-key function-key-map "\e[C"    [C-right])
+(define-key function-key-map "\e\e[D"  [M-left])
+(define-key function-key-map "\e\e[C"  [M-right])
+(define-key function-key-map "\e\e[A"  [M-up])
+(define-key function-key-map "\e\e[B"  [M-down])
+
+
+;;; konsole-binds.el ends here
